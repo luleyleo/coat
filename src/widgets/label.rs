9cb0f155359a0bdf462355d933771bd04190ec23
Leopold Luley <git@leopoldluley.de>
2021-02-06 18:22:29 +0100

    Simplify Label.

--- src/widgets/label.rs
+++ src/widgets/label.rs
@@ -1,17 +1,3 @@
-// Copyright 2019 The Druid Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //! A label widget.
 
 use crate::render::prelude::*;
@@ -67,127 +53,45 @@ impl Label {
         }
     }
 
+    pub fn text_color(mut self, color: Color) -> Self {
+        self.layout.set_text_color(color);
+        self
+    }
+
+    pub fn text_size(mut self, size: f64) -> Self {
+        self.layout.set_text_size(size);
+        self
+    }
+
+    pub fn font(mut self, font: FontDescriptor) -> Self {
+        self.layout.set_font(font);
+        self
+    }
+
+    pub fn line_break_mode(mut self, mode: LineBreaking) -> Self {
+        self.line_break_mode = mode;
+        self
+    }
+
+    pub fn text_alignment(mut self, alignment: TextAlignment) -> Self {
+        self.layout.set_text_alignment(alignment);
+        self
+    }
+
     #[track_caller]
     pub fn build(self, cx: &mut Cx) {
         let caller = Location::caller().into();
         cx.render_object(caller, self, |_| {});
     }
+}
 
-    /// Builder-style method for setting the text color.
-    ///
-    /// The argument can be either a `Color` or a [`Key<Color>`].
-    ///
-    /// [`Key<Color>`]: ../struct.Key.html
-    pub fn with_text_color(mut self, color: impl Into<KeyOrValue<Color>>) -> Self {
-        self.set_text_color(color);
-        self
-    }
-
-    /// Builder-style method for setting the text size.
-    ///
-    /// The argument can be either an `f64` or a [`Key<f64>`].
-    ///
-    /// [`Key<f64>`]: ../struct.Key.html
-    pub fn with_text_size(mut self, size: impl Into<KeyOrValue<f64>>) -> Self {
-        self.set_text_size(size);
-        self
-    }
-
-    /// Builder-style method for setting the font.
-    ///
-    /// The argument can be a [`FontDescriptor`] or a [`Key<FontDescriptor>`]
-    /// that refers to a font defined in the [`Env`].
-    ///
-    /// [`Env`]: ../struct.Env.html
-    /// [`FontDescriptor`]: ../struct.FontDescriptor.html
-    /// [`Key<FontDescriptor>`]: ../struct.Key.html
-    pub fn with_font(mut self, font: impl Into<KeyOrValue<FontDescriptor>>) -> Self {
-        self.set_font(font);
-        self
-    }
-
-    /// Builder-style method to set the [`LineBreaking`] behaviour.
-    ///
-    /// [`LineBreaking`]: enum.LineBreaking.html
-    pub fn with_line_break_mode(mut self, mode: LineBreaking) -> Self {
-        self.set_line_break_mode(mode);
-        self
-    }
-
-    /// Builder-style method to set the [`TextAlignment`].
-    ///
-    /// [`TextAlignment`]: enum.TextAlignment.html
-    pub fn with_text_alignment(mut self, alignment: TextAlignment) -> Self {
-        self.set_text_alignment(alignment);
-        self
-    }
-
-    /// Set the text color.
-    ///
-    /// The argument can be either a `Color` or a [`Key<Color>`].
-    ///
-    /// If you change this property, you are responsible for calling
-    /// [`request_layout`] to ensure the label is updated.
-    ///
-    /// [`request_layout`]: ../struct.EventCtx.html#method.request_layout
-    /// [`Key<Color>`]: ../struct.Key.html
-    pub fn set_text_color(&mut self, color: impl Into<KeyOrValue<Color>>) {
-        self.layout.set_text_color(color);
-    }
-
-    /// Set the text size.
-    ///
-    /// The argument can be either an `f64` or a [`Key<f64>`].
-    ///
-    /// If you change this property, you are responsible for calling
-    /// [`request_layout`] to ensure the label is updated.
-    ///
-    /// [`request_layout`]: ../struct.EventCtx.html#method.request_layout
-    /// [`Key<f64>`]: ../struct.Key.html
-    pub fn set_text_size(&mut self, size: impl Into<KeyOrValue<f64>>) {
-        self.layout.set_text_size(size);
-    }
-
-    /// Set the font.
-    ///
-    /// The argument can be a [`FontDescriptor`] or a [`Key<FontDescriptor>`]
-    /// that refers to a font defined in the [`Env`].
-    ///
-    /// If you change this property, you are responsible for calling
-    /// [`request_layout`] to ensure the label is updated.
-    ///
-    /// [`request_layout`]: ../struct.EventCtx.html#method.request_layout
-    /// [`Env`]: ../struct.Env.html
-    /// [`FontDescriptor`]: ../struct.FontDescriptor.html
-    /// [`Key<FontDescriptor>`]: ../struct.Key.html
-    pub fn set_font(&mut self, font: impl Into<KeyOrValue<FontDescriptor>>) {
-        self.layout.set_font(font);
-    }
-
-    /// Set the [`LineBreaking`] behaviour.
-    ///
-    /// If you change this property, you are responsible for calling
-    /// [`request_layout`] to ensure the label is updated.
-    ///
-    /// [`request_layout`]: ../struct.EventCtx.html#method.request_layout
-    /// [`LineBreaking`]: enum.LineBreaking.html
-    pub fn set_line_break_mode(&mut self, mode: LineBreaking) {
-        self.line_break_mode = mode;
-    }
-
-    /// Set the [`TextAlignment`] for this layout.
-    ///
-    /// [`TextAlignment`]: enum.TextAlignment.html
-    pub fn set_text_alignment(&mut self, alignment: TextAlignment) {
-        self.layout.set_text_alignment(alignment);
-    }
-
+impl Label {
     /// Draw this label's text at the provided `Point`, without internal padding.
     ///
     /// This is a convenience for widgets that want to use Label as a way
     /// of managing a dynamic or localized string, but want finer control
     /// over where the text is drawn.
-    pub fn draw_at(&self, ctx: &mut PaintCtx, origin: impl Into<Point>) {
+    fn draw_at(&self, ctx: &mut PaintCtx, origin: impl Into<Point>) {
         debug_assert!(
             self.layout.layout().is_some(),
             "TextLayout::draw called without rebuilding layout object. Text was '{}'",
@@ -203,7 +107,7 @@ impl Label {
     }
 
     /// Return the offset of the first baseline relative to the bottom of the widget.
-    pub fn baseline_offset(&self) -> f64 {
+    fn baseline_offset(&self) -> f64 {
         let text_metrics = self.layout.layout_metrics();
         text_metrics.size.height - text_metrics.first_baseline
     }
